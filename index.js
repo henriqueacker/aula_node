0e2bf05147617de87d58aa2594f874ec19b48c37
Henrique <henriqueacker@gmail.com>
2023-10-06 14:18:16 -0300

    aula de formulario

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express') //import do express
 const app = express(); //iniciando o express
 app.set('view engine', 'ejs') // Defenindo o render de HTML
-app.use(express.static('public')) // Definindo public
+app.use(express.static('public')) // Definindo public para arquivos estaticos
+
+//Capturar os dados do formulario e receber json
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
 //ROTAS
 app.get('/', (req, res)=>{
@@ -11,13 +15,17 @@ app.get('/', (req, res)=>{
     })
 })
 
-app.get('/sobre', (req, res)=>{
-    res.render('sobre')
+app.get('/perguntar', (req, res)=>{
+    res.render('layout', { pageContent: 'perguntar.ejs' });
 })
 
 
 
-
+app.post('/salvarPergunta', (req, res)=>{
+    const titulo = req.body.titulo
+    const descricao = req.body.descricao
+    res.send(titulo)
+})
 
 
 
